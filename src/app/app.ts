195a6112f71ac417a652418cf42327a75606fa28
Sanjeev Singh <sksedu84@gmail.com>
2026-08-12 17:17:02 -0400

    Processing component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component} from '@angular/core';
 import { Header } from './header/header';
 import { RouterOutlet } from '@angular/router';
 import { Processing } from './processing/processing';
